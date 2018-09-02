3207b0579b25c63d6b5e9b375a691ecdb8c37417
Prasad <prasadT@emea.travelex.net>
2018-09-02 21:55:00 +0530

    updated resource

---
+++ src/main/java/com/techstack/resource/AutoCommitResource.java
@@ -0,0 +1,13 @@
+package com.techstack.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AutoCommitResource {
+
+	@GetMapping ("/callme")
+	public String callme() {
+		return "Successfull !!";
+	}
+}
